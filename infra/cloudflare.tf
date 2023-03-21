d7d71f6952778ccc3c23b7d9c70cebe5535a90ce
Marcin Białoń <marcin@bialon.net>
2023-03-21 21:09:13 +0100

    add infra config (#116)

---
+++ infra/cloudflare.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.28.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.api_token
+}
+
+variable "api_token" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "zone_id" {
+  type = string
+}
+
+resource "cloudflare_pages_project" "kb" {
+  account_id        = var.account_id
+  name              = "mbialon-kb"
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner                         = "mbialon"
+      repo_name                     = "kb"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_branch_includes       = ["*"]
+    }
+  }
+
+  build_config {
+    build_command   = "mkdocs build"
+    destination_dir = "site"
+  }
+
+  deployment_configs {
+    preview {
+      environment_variables = {
+        PYTHON_VERSION = "3.7"
+      }
+    }
+    production {
+      environment_variables = {
+        PYTHON_VERSION = "3.7"
+      }
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "kb" {
+  account_id   = var.account_id
+  project_name = cloudflare_pages_project.kb.name
+  domain       = "kb.bialon.net"
+}
+
+resource "cloudflare_record" "kb" {
+  zone_id = var.zone_id
+  type    = "CNAME"
+  name    = "kb.bialon.net"
+  value   = cloudflare_pages_project.kb.subdomain
+  proxied = true
+}
